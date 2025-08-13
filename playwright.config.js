7f4e47786e012b97c69468e138398cac5a5d9961
Rubeet <rubeet.husnain16@gmail.com>
2025-08-13 16:01:51 +0500

    Fix Playwright report generation in CI

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
+});
